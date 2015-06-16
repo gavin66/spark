c0d0ad100c9e11d7cc8f287fd96f8ca57ed59a73
gavin <tianyu_0723@hotmail.com>
2015-06-16 08:46:52 +0800

    gavin branch first commit

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () {return view('welcome');});
+Route::get('home', 'HomeController@index');
+
+Route::controllers([
+    'auth' => 'Auth\AuthController',
+    'password' => 'Auth\PasswordController',
+]);
--- config/database.php
+++ config/database.php
@@ -52,15 +52,15 @@ return [
             'prefix'   => '',
         ],
 
-        'mysql' => [
+        'spark_mysql' => [
             'driver'    => 'mysql',
-            'host'      => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'forge'),
-            'username'  => env('DB_USERNAME', 'forge'),
-            'password'  => env('DB_PASSWORD', ''),
+            'host'      => env('DB_HOST', '121.42.158.246'),
+            'database'  => env('DB_DATABASE', 'spark'),
+            'username'  => env('DB_USERNAME', 'root'),
+            'password'  => env('DB_PASSWORD', 'Spark654231'),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
-            'prefix'    => '',
+            'prefix'    => 'sp_',
             'strict'    => false,
         ],
 
